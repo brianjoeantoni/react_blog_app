6d183595bd13d29c8ccfe9d9079e5023d5a41d28
Brian Joe Antoni <brianjoeantoni@gmail.com>
2023-07-04 12:54:17 +0700

    deploy

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,13 @@ import DataContext from "./context/DataContext";
 
 const Home = () => {
   const { searchResults, fetchError, isLoading } = useContext(DataContext);
+  const msg = `${fetchError} \nPlease run: "npx json-server -p 3500 -w data/db.json" on VSCode`;
+
 
   return (
     <main className="Home">
       {isLoading && <p className="statusMsg">Loading posts...</p> }
-      {!isLoading && fetchError && <p className="statusMsg" style={{color: "red"}}>{fetchError}</p> }
+      {!isLoading && fetchError && <p className="statusMsg" style={{color: "red"}}>{msg}</p> }
       {!isLoading && !fetchError && (searchResults.length ? <Feed posts={searchResults}/> : <p className="statusMsg">No posts to display.</p> )}
     </main>
   );
